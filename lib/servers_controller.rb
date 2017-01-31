a18fdcda3bb83282ea618660008dac3e728b4ae3
Jye Lee <jlee@cloudpassage.com>
2017-01-31 10:19:37 -0800

    add 3rd level of server detection via ec2

--- lib/servers_controller.rb
+++ lib/servers_controller.rb
@@ -2,11 +2,13 @@
 require 'json'
 require_relative 'validator'
 require_relative 'api_controller'
+require_relative 'ec2'
 
 # Returns server information
 class ServersController < ApiController
   def initialize
     super
+    @servers = index['servers']
   end
 
   def index(filters = nil)
@@ -23,21 +25,48 @@ class ServersController < ApiController
     data
   end
 
-  def build_ec2_data
-    servers = index['servers']
-    servers.each do |e|
-      next if e.key? 'aws_ec2'
-      ec2_data = e['server_label'].match(/(^\d+)_(i.+)/) unless e['server_label'].nil?
-      if ec2_data
-        e['aws_ec2'] = { 'ec2_account_id' => ec2_data[1], 'ec2_instance_id' => ec2_data[2] }
-      end
-    end
-
-    servers
+  def halo_ips
+    @servers.map { |e| e['connecting_ip_address'] }
   end
 
-  def ec2_servers(account_id)
-    servers = build_ec2_data.select! { |srv| srv.key? 'aws_ec2' }
-    servers.select! { |srv| srv['aws_ec2']['ec2_account_id'].eql? account_id.to_s }
+  def ec2_servers
+    options = [{ name: 'instance-state-name', values: ['running'] }]
+    EC2.client.describe_instances(filters: options)
+  end
+
+  def build_ec2_data
+    @servers.each do |srv|
+      next if srv.key? 'aws_ec2'
+      ec2_data = srv['server_label'].match(/(^\d+)_(i.+)/) unless srv['server_label'].nil?
+      if ec2_data
+        srv['aws_ec2'] = {
+          'ec2_account_id' => ec2_data[1],
+          'ec2_instance_id' => ec2_data[2]
+        }
+      end
+
+      build_aws_data(ec2_servers, srv, halo_ips)
+    end
+
+    @servers
+  end
+
+  def build_aws_data(ec2_index, halo_server, ips)
+    ec2_index['reservations'].each do |reservation|
+      reservation['instances'].each do |instance|
+        interface = instance.network_interfaces.detect { |e| e.owner_id }
+        next if interface.nil?
+        if ips.include? instance.public_ip_address
+          halo_server['aws_ec2'] = {
+            'ec2_account_id' => interface.owner_id,
+            'ec2_instance_id' => instance.instance_id
+          }
+        end
+      end
+    end
+  end
+
+  def filtered_servers(account_id)
+    build_ec2_data.select! { |srv| srv['aws_ec2']['ec2_account_id'].eql? account_id.to_s }
   end
 end
